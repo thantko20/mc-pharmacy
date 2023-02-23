d05a01576670439ca5d4ac97a2c2f808eef0388b
Thant Ko Zaw <thantmkou10@gmail.com>
2023-02-23 23:04:40 +0630

    Add some ui to headers

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,9 +1,13 @@
-import { Box, Stack } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import { Box, Button, Stack, Typography } from '@mui/material';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import { SectionContainer } from './SectionContainer';
+import { useAuth } from '@/features/auth/components/AuthProvider';
 
 export const Header = () => {
+  const { user, logoutFn } = useAuth();
+  const navigate = useNavigate();
+
   return (
     <header>
       <SectionContainer>
@@ -11,6 +15,28 @@ export const Header = () => {
           <Box fontSize='2rem' fontWeight={600}>
             MC Pharmacy
           </Box>
+          <Stack direction='row' alignItems='center' spacing={2}>
+            {user ? (
+              <>
+                <Typography variant='subtitle1' component='span'>
+                  {user.name}
+                </Typography>
+                <Button color='error' onClick={logoutFn}>
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button onClick={() => navigate('/auth/login')}>Login</Button>
+                <Button
+                  variant='contained'
+                  onClick={() => navigate('/auth/register')}
+                >
+                  Register
+                </Button>
+              </>
+            )}
+          </Stack>
         </Stack>
       </SectionContainer>
     </header>
