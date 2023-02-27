ad1fbb4f51f2d013b575c1a8214cd61755523cbf
Thant Ko Zaw <thantmkou10@gmail.com>
2023-02-27 13:20:29 +0630

    Refactor and add local controls

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -8,25 +8,173 @@ import Twilio, {
   LocalAudioTrack,
   RemoteAudioTrack,
   Track,
+  LocalParticipant,
 } from 'twilio-video';
-import { useState, useEffect, useRef } from 'react';
-import { Box, Button, Stack } from '@mui/material';
+import { useState, useEffect, useRef, useCallback } from 'react';
+import { Box, Button, IconButton, Stack } from '@mui/material';
 import { SectionContainer } from '@/components/SectionContainer';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { socket } from '@/lib/socket-io';
 import { useAuth } from '@/features/auth/components/AuthProvider';
 import { TUser } from '@/features/auth/types';
+import {
+  CallEnd,
+  Mic,
+  MicOff,
+  Videocam,
+  VideocamOff,
+} from '@mui/icons-material';
 
 type TVideoTracks = (LocalVideoTrack | RemoteVideoTrack | null)[];
 
 type TAudioTracks = (LocalAudioTrack | RemoteAudioTrack | null)[];
-const Participant = ({
-  participant,
-  name,
+
+const useRoom = ({
+  roomName,
+  token,
+  calleeId,
 }: {
+  roomName: string;
+  token: string;
+  calleeId?: string;
+}) => {
+  const [room, setRoom] = useState<Room | null>(null);
+  const [isRoomConnecting, setIsRoomConnecting] = useState(true);
+  const { user } = useAuth();
+
+  const navigate = useNavigate();
+
+  const connectRoom = useCallback(async () => {
+    try {
+      const room = await Twilio.connect(token, {
+        name: roomName,
+      });
+      setRoom(room);
+      if (room.participants.size === 0) {
+        socket.emit('start-call', {
+          callerId: user?._id,
+          calleeId: calleeId,
+          roomName: room.name,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsRoomConnecting(false);
+    }
+  }, [roomName, token]);
+
+  const disconnectRoom = () => {
+    setRoom((currentRoom) => {
+      if (currentRoom && currentRoom.localParticipant.state === 'connected') {
+        currentRoom.localParticipant.tracks.forEach((track) =>
+          track.unpublish(),
+        );
+        currentRoom.disconnect();
+        return null;
+      }
+      return currentRoom;
+    });
+  };
+
+  const hangUp = () => {
+    disconnectRoom();
+    navigate('/test');
+  };
+
+  useEffect(() => {
+    connectRoom();
+
+    return () => {
+      disconnectRoom();
+    };
+  }, []);
+
+  return {
+    room,
+    hangUp,
+    isRoomConnecting,
+  };
+};
+
+const useParticipants = ({ room }: { room: Room | null }) => {
+  const [participants, setParticipants] = useState<TwilioParticipant[]>([]);
+
+  const participantConnected = (participant: TwilioParticipant) => {
+    setParticipants((prevParticipants) => [...prevParticipants, participant]);
+  };
+
+  const participantDisconnected = (participant: TwilioParticipant) => {
+    setParticipants((prevParticipants) =>
+      prevParticipants.filter((p) => p !== participant),
+    );
+  };
+
+  useEffect(() => {
+    if (room) {
+      room.on('participantConnected', participantConnected);
+      room.on('participantDisconnected', participantDisconnected);
+      room.participants.forEach(participantConnected);
+    } else {
+      setParticipants([]);
+    }
+  }, [room]);
+
+  return {
+    participants,
+  };
+};
+
+const useLocalParticipantControls = (localParticipant?: LocalParticipant) => {
+  const [isVideoOff, setIsVideoOff] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+
+  const toggleVideo = () => {
+    if (localParticipant) {
+      localParticipant.videoTracks.forEach((publication) => {
+        const track = publication.track;
+        if (track.isEnabled) {
+          track.disable();
+          setIsVideoOff(true);
+        } else {
+          track.enable();
+          setIsVideoOff(false);
+        }
+      });
+    }
+  };
+
+  const toggleAudio = () => {
+    if (localParticipant) {
+      localParticipant.audioTracks.forEach((publication) => {
+        const track = publication.track;
+        if (track.isEnabled) {
+          track.disable();
+          setIsMuted(true);
+        } else {
+          track.enable();
+          setIsMuted(false);
+        }
+      });
+    }
+  };
+
+  return {
+    toggleVideo,
+    toggleAudio,
+    isVideoOff,
+    isMuted,
+  };
+};
+
+type ParticipantProps = {
   participant: TwilioParticipant;
   name: string;
-}) => {
+  onToggleVideo?: (...args: any) => void;
+  onToggleAudio?: (...args: any) => void;
+};
+
+const Participant = ({ participant, name }: ParticipantProps) => {
   const [videoTracks, setVideoTracks] = useState<TVideoTracks>([]);
   const [audioTracks, setAudioTracks] = useState<TAudioTracks>([]);
 
@@ -110,18 +258,9 @@ const Participant = ({
 };
 
 const RoomPage = () => {
-  const [room, setRoom] = useState<Room | null>(null);
-  const [participants, setParticipants] = useState<TwilioParticipant[]>([]);
-
   const { user } = useAuth();
-
   const location = useLocation();
-  const navigate = useNavigate();
   const { roomName } = useParams();
-
-  if (!roomName || typeof roomName !== 'string') {
-    navigate(-1);
-  }
 
   const { token, otherParticipant, isMeCaller } = location.state as {
     token: string;
@@ -129,75 +268,23 @@ const RoomPage = () => {
     isMeCaller?: boolean;
   };
 
+  if (!roomName || !token || !otherParticipant) {
+    return <div>Invalid Blah blah</div>;
+  }
+
+  const { room, hangUp } = useRoom({
+    roomName: roomName as string,
+    token,
+    calleeId: otherParticipant._id,
+  });
+  const { participants } = useParticipants({ room });
+
+  const { toggleVideo, isVideoOff, toggleAudio, isMuted } =
+    useLocalParticipantControls(room?.localParticipant);
+
   if (isMeCaller && !otherParticipant?._id) {
     return <div>Need callee id</div>;
   }
-
-  const disconnectRoom = () => {
-    setRoom((currentRoom) => {
-      if (currentRoom && currentRoom.localParticipant.state === 'connected') {
-        setParticipants([]);
-        currentRoom.localParticipant.tracks.forEach((track) =>
-          track.unpublish(),
-        );
-        currentRoom.disconnect();
-        return null;
-      }
-      return currentRoom;
-    });
-  };
-
-  const hangUp = () => {
-    disconnectRoom();
-    navigate('/test');
-  };
-
-  useEffect(() => {
-    const participantConnected = (participant: TwilioParticipant) => {
-      setParticipants((prevParticipants) => [...prevParticipants, participant]);
-    };
-
-    const participantDisconnected = (participant: TwilioParticipant) => {
-      setParticipants((prevParticipants) =>
-        prevParticipants.filter((p) => p !== participant),
-      );
-    };
-
-    const connectRoom = async () => {
-      try {
-        const room = await Twilio.connect(token, {
-          name: roomName,
-        });
-
-        setRoom(room);
-        if (isMeCaller) {
-          socket.emit('start-call', {
-            callerId: user?._id,
-            calleeId: otherParticipant!._id,
-            roomName: room.name,
-          });
-        }
-        room.on('participantConnected', participantConnected);
-        room.on('participantDisconnected', participantDisconnected);
-        room.participants.forEach(participantConnected);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    connectRoom();
-
-    return () => {
-      setRoom((currentRoom) => {
-        if (currentRoom && currentRoom.localParticipant.state === 'connected') {
-          currentRoom.disconnect();
-          return null;
-        } else {
-          return currentRoom;
-        }
-      });
-    };
-  }, []);
 
   return (
     <SectionContainer>
@@ -224,9 +311,27 @@ const RoomPage = () => {
                   />
                 ) : null}
               </Stack>
-              <Button variant='contained' color='error' onClick={hangUp}>
-                Hangup
-              </Button>
+              <Stack direction='row' alignItems='center' spacing={2}>
+                <Button
+                  onClick={toggleVideo}
+                  color='secondary'
+                  variant='contained'
+                  aria-label='toggle camera'
+                >
+                  {isVideoOff ? <VideocamOff /> : <Videocam />}
+                </Button>
+                <Button
+                  onClick={toggleAudio}
+                  color='secondary'
+                  variant='contained'
+                  aria-label='toggle microphone'
+                >
+                  {isMuted ? <MicOff /> : <Mic />}
+                </Button>
+                <Button variant='contained' color='error' onClick={hangUp}>
+                  <CallEnd />
+                </Button>
+              </Stack>
             </Stack>
           ) : null}
         </div>
